4dbe14a4c5ffa3a0d57fac9249b2ef89c41d2598
Igor Popov <ipopov4422@gmail.com>
2023-06-24 09:57:07 +0100

    Settings for CRACO and some styles

---
+++ craco.config.js
@@ -0,0 +1,39 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { ModuleFederationPlugin } = require('webpack').container
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const deps = require('./package.json').dependencies
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    port: 3002
+  },
+  webpack: {
+    plugins: [
+      new ModuleFederationPlugin({
+        name: 'app1',
+        filename: 'remoteEntry.js',
+        exposes: {
+          './app1': './src/App'
+        },
+        shared: {
+          ...deps,
+          react: {
+            singleton: true,
+            requiredVersion: deps['react']
+          },
+          'react-dom': {
+            singleton: true,
+            requiredVersion: deps['react-dom']
+          }
+        }
+      })
+    ],
+    configure: {
+      output: {
+        publicPath: 'auto'
+      }
+    }
+  }
+}
