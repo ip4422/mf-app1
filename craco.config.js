39be64d94d1a72746c2000043a01d8ce211339b1
Igor Popov <ipopov4422@gmail.com>
2023-07-09 11:30:33 +0100

    Some fix for publicPath

--- craco.config.js
+++ craco.config.js
@@ -22,7 +22,14 @@ module.exports = {
     ],
     configure: {
       output: {
-        publicPath: 'auto'
+        // We have to configure the output dir mode because
+        // we using nested paths for the pages and in this case webpack will
+        // try to fetch our main.js path from this publicPath
+        // `http://localhost:3002/main.js` instead of
+        // `http://localhost:3003/somePageName/main.js`
+        // finalizing slash is important!
+
+        publicPath: 'http://localhost:3002/'
       }
     }
   }
