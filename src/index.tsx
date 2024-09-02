f3c569be9f9798d30c847e7e9855fa67dec9a60a
Igor Popov <ipopov4422@gmail.com>
2024-09-02 16:08:24 +0100

    Add documentation

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,6 @@
+// Module Federation allows different applications (microfrontends)
+// to share modules at runtime. To achieve this, Webpack dynamically
+// loads these shared modules. This ensures that the application only
+// starts after all federated modules are properly initialized.
 // @ts-ignore
 import('./bootstrap')
