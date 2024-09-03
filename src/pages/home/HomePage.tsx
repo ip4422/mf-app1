f6ab7bd26ace315cde7981b1ae4e488c07a68875
Igor Popov <ipopov4422@gmail.com>
2024-09-03 20:36:53 +0100

    Add routes

---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,11 @@
+export const HomePage: React.FC = () => {
+  return (
+    <>
+      <section className='my-8'>some content</section>
+
+      <section className='my-8'>some content</section>
+    </>
+  )
+}
+
+export default HomePage
