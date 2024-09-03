b3bec3bd46a04341d881d179a714d12e24cbb65c
Igor Popov <ipopov4422@gmail.com>
2024-09-03 20:35:54 +0100

    Standalone app ID settings

--- src/bootstrap.tsx
+++ src/bootstrap.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
-import App from './App'
+import App from 'app/root/App'
 import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const root = ReactDOM.createRoot(
+  document.getElementById('_mfe-app1-root') as HTMLElement
+)
 root.render(
   <React.StrictMode>
     <App />
