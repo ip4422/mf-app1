d0833dfabf2c0d83e97eca2492c6578fe7b29d6e
Igor Popov <ipopov4422@gmail.com>
2024-09-10 10:07:37 +0100

    Fix layout

--- src/app/routes/ui/RootLayout.tsx
+++ src/app/routes/ui/RootLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom'
 
 const RootLayout: React.FC = () => {
   return (
-    <div className='flex flex-col min-h-screen bg-gray-100 text-gray-900'>
+    <div className='flex flex-col bg-gray-100 text-gray-900'>
       <main className='container mx-auto p-4'>
         <Outlet />
       </main>
